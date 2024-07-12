8a4c7d289e2c06078f4e5a515c54f090e318a1d3
Victoria Lauri <124601042+VictoriaLauri@users.noreply.github.com>
2024-07-12 11:29:56 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="copyright">
+            <p>Copyright 2024 Victoria Lauri</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo () {
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Promo of the Month</h1>
+            </div>
+            <div>
+                <h2>50% off on all items</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
